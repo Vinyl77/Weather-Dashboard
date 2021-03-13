7890cf5662771311e24547fe8a57b2bee9bb5167
Richard Yvarra <richard.yvarra@gmail.com>
2021-03-12 19:25:49 -0500

    getting started

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
 
+    var searchHistory = [];
+
+       function getItems(){
+            var savedCities
+           JSON.parse(localStorage.getItem("searchHistory"));
+              if (savedCities !== null){
+                  searchHistory = savedCities;
+              };
+           
+          for(i = 0; 1< searchHistory.length; i++){
+              if(i == 8){
+                  break;
+              }
+
+            locationListButton = $("<a>").attr({
+                class: "list-group-item list-group-item-action",
+                href: "#"
+             });
+
+             locationListButton.text(searchHistory[i]);
+             $(".list-group").append(locationListButton);
+
+          }
+       };
     
 })
